bedb5123576738301dfe4ea2d89aed0d1566d991
Michael Cummings <dragonrun1@gmail.com>
2012-01-01 01:49:23 -0800

    Fix for charContract and corpContract not handling empty dateAccepted and dateCompleted fields in XML correctly.

--- class/api/corp/corpContracts.php
+++ class/api/corp/corpContracts.php
@@ -46,7 +46,7 @@ if (count(get_included_files()) < 2) {
   } else {
     fwrite(STDERR, $mess);
     exit(1);
-  };
+  }
 };
 /**
  * Class used to fetch and store corp StarbaseList API.
@@ -73,5 +73,63 @@ class corpContracts extends ACorp {
     $this->api = str_replace($this->section, '', __CLASS__);
     parent::__construct($params);
   }// function __construct
+  /**
+   * Parsers the XML from API.
+   *
+   * Most common API style is a simple <rowset>. Journals are a little more
+   * complex because of need to do walking back for older records.
+   *
+   * @return bool Returns TRUE if XML was parsed correctly, FALSE if not.
+   */
+  protected function parserAPI() {
+    if (Logger::getLogger('yapeal')->isDebugEnabled()) {
+      Logger::getLogger('yapeal')->trace(__METHOD__);
+    };
+    $tableName = YAPEAL_TABLE_PREFIX . $this->section . $this->api;
+    // Get a new query instance with autoStore off.
+    $qb = new YapealQueryBuilder($tableName, YAPEAL_DSN);
+    // Set any column defaults needed.
+    $qb->setDefault('ownerID', $this->ownerID);
+    try {
+      while ($this->xr->read()) {
+        switch ($this->xr->nodeType) {
+          case XMLReader::ELEMENT:
+            switch ($this->xr->localName) {
+              case 'row':
+                $row = array();
+                // Walk through attributes and add them to row.
+                while ($this->xr->moveToNextAttribute()) {
+                  // Allow YapealQueryBuilder to handle NULL columns.
+                  if (($this->xr->name == 'dateAccepted'
+                    || $this->xr->name == 'dateCompleted')
+                    && $this->xr->value == '') {
+                    continue;
+                  };
+                  $row[$this->xr->name] = $this->xr->value;
+                };// while $this->xr->moveToNextAttribute() ...
+                $qb->addRow($row);
+                break;
+            };// switch $this->xr->localName ...
+            break;
+          case XMLReader::END_ELEMENT:
+            if ($this->xr->localName == 'result') {
+              if (count($qb) > 0) {
+                $qb->store();
+              };// if $this->rowCount ...
+              $qb = NULL;
+              return TRUE;
+            };// if $this->xr->localName == 'row' ...
+            break;
+        };// switch $this->xr->nodeType
+      };// while $xr->read() ...
+    }
+    catch (ADODB_Exception $e) {
+      Logger::getLogger('yapeal')->error($e);
+      return FALSE;
+    }
+    $mess = 'Function ' . __FUNCTION__ . ' did not exit correctly' . PHP_EOL;
+    Logger::getLogger('yapeal')->warn($mess);
+    return FALSE;
+  }// function parserAPI
 }
 ?>

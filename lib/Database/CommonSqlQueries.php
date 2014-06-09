eca362833e3d91c77e5e9f8d06856f55a96f68e2
Michael Cummings <mgcummings@yahoo.com>
2014-06-08 21:59:11 -0700

    Added Database/CommonSqlQueries class to centralize all SQL queries into.

---
+++ lib/Database/CommonSqlQueries.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Contains CommonSqlQueries class.
+ *
+ * PHP version 5.3
+ *
+ * LICENSE:
+ * This file is part of 1.1.x-WIP
+ * Copyright (C) 2014 Michael Cummings
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * You should be able to find a copy of this license in the LICENSE.md file. A copy of the GNU GPL should also be
+ * available in the GNU-GPL.md file.
+ *
+ * @copyright 2014 Michael Cummings
+ * @license   http://www.gnu.org/copyleft/lesser.html GNU LGPL
+ * @author    Michael Cummings <mgcummings@yahoo.com>
+ */
+namespace Yapeal\Database;
+
+/**
+ * Class CommonSqlQueries
+ */
+class CommonSqlQueries
+{
+    /**
+     * @param string $databaseName
+     * @param string $tablePrefix
+     */
+    public function __construct($databaseName, $tablePrefix)
+    {
+        $this->databaseName = $databaseName;
+        $this->tablePrefix = $tablePrefix;
+    }
+    /**
+     * @return string
+     */
+    public function getActiveApis()
+    {
+        return sprintf(
+            'SELECT * FROM "%1$s"."%2$sutilEveApi" WHERE "isActive"=1 ORDER BY RAND()',
+            $this->databaseName,
+            $this->tablePrefix
+        );
+    }
+    /**
+     * @return string
+     */
+    public function getActiveRegisteredKeys()
+    {
+        return sprintf(
+            'SELECT "keyID","vCode" FROM "%1$s"."%2$sutilRegisteredKey" WHERE "isActive"=1',
+            $this->databaseName,
+            $this->tablePrefix
+        );
+    }
+    /**
+     * @var string
+     */
+    private $databaseName;
+    /**
+     * @var string
+     */
+    private $tablePrefix;
+}

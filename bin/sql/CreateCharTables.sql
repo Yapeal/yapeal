64429bf32bc6aa711c2a8f977cfb0cfada0c4e56
Michael Cummings <mgcummings@yahoo.com>
2014-06-08 21:23:33 -0700

    Updated Create Table for charNotifications which fixes issue #10 in 1.1.x-WIP as well.

--- bin/sql/CreateCharTables.sql
+++ bin/sql/CreateCharTables.sql
@@ -372,6 +372,7 @@ CREATE TABLE IF NOT EXISTS "{database}"."{table_prefix}charNotifications" (
     "notificationID" BIGINT(20) UNSIGNED  NOT NULL,
     "read"           TINYINT(1)           NOT NULL,
     "senderID"       BIGINT(20) UNSIGNED  NOT NULL,
+    "senderName" CHAR(50) DEFAULT NULL,
     "sentDate"       DATETIME             NOT NULL,
     "typeID"         SMALLINT(5) UNSIGNED NOT NULL,
     PRIMARY KEY ("ownerID", "notificationID")

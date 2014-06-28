1cb08bdc421e50fcde4f84ba923eef13ac93eeae
stephenmg12 <stephenmg12@gmail.com>
2014-06-28 16:33:01 -0500

    adding Database/Char/AbstractCharSection.php and modified Database/Eve/CharacterInfoPrivate.php to use it

--- lib/Database/Eve/CharacterInfoPrivate.php
+++ lib/Database/Eve/CharacterInfoPrivate.php
@@ -29,145 +29,20 @@
  */
 namespace Yapeal\Database\Eve;
 
-use PDO;
 use PDOException;
-use Yapeal\Database\AbstractCommonEveApi;
-use Yapeal\Database\AttributesDatabasePreserver;
-use Yapeal\Database\DatabasePreserverInterface;
-use Yapeal\Database\ValuesDatabasePreserver;
-use Yapeal\Xml\EveApiPreserverInterface;
-use Yapeal\Xml\EveApiReadWriteInterface;
-use Yapeal\Xml\EveApiRetrieverInterface;
-use Yapeal\Xml\EveApiXmlModifyInterface;
+use Yapeal\Database\Char\AbstractCharSection;
 
 /**
  * Class CharacterInfoPrivate
  */
-class CharacterInfoPrivate extends AbstractCommonEveApi
+class CharacterInfoPrivate extends AbstractCharSection
 {
-    /**
-     * @param EveApiReadWriteInterface $data
-     * @param EveApiRetrieverInterface $retrievers
-     * @param EveApiPreserverInterface $preservers
-     * @param int                      $interval
-     */
-    public function autoMagic(
-        EveApiReadWriteInterface $data,
-        EveApiRetrieverInterface $retrievers,
-        EveApiPreserverInterface $preservers,
-        $interval
-    ) {
-        $this->getLogger()
-             ->info(
-                 sprintf(
-                     'Starting autoMagic for %1$s/%2$s',
-                     $this->getSectionName(),
-                     $this->getApiName()
-                 )
-             );
-        $active = $this->getActiveCharacters();
-        if (empty($active)) {
-            $this->getLogger()
-                 ->info('No active characters found');
-            return;
-        }
-        $aPreserver = new AttributesDatabasePreserver(
-            $this->getPdo(),
-            $this->getLogger(),
-            $this->getCsq()
-        );
-        $vPreserver = new ValuesDatabasePreserver(
-            $this->getPdo(),
-            $this->getLogger(),
-            $this->getCsq()
-        );
-        foreach ($active as $char) {
-            /**
-             * @var EveApiReadWriteInterface|EveApiXmlModifyInterface $data
-             */
-            $data->setEveApiSectionName(strtolower($this->getSectionName()))
-                ->setEveApiName('CharacterInfo');
-            if ($this->cacheNotExpired(
-                $this->getApiName(),
-                $this->getSectionName(),
-                $char['characterID']
-            )
-            ) {
-                continue;
-            }
-            $data->setEveApiArguments($char)
-                 ->setEveApiXml();
-            $retrievers->retrieveEveApi($data);
-            if ($data->getEveApiXml() === false) {
-                $mess = sprintf(
-                    'Could NOT retrieve any data from Eve API %1$s/%2$s for %3$s',
-                    strtolower($this->getSectionName()),
-                    $this->getApiName(),
-                    $char['characterID']
-                );
-                $this->getLogger()
-                     ->debug($mess);
-                continue;
-            }
-            $this->xsltTransform($data);
-            if ($this->isInvalid($data)) {
-                $mess = sprintf(
-                    'The data retrieved from Eve API %1$s/%2$s for %3$s is invalid',
-                    strtolower($this->getSectionName()),
-                    $this->getApiName(),
-                    $char['characterID']
-                );
-                $this->getLogger()
-                     ->warning($mess);
-                $data->setEveApiName('Invalid' . $this->getApiName());
-                $preservers->preserveEveApi($data);
-                continue;
-            }
-            $preservers->preserveEveApi($data);
-            $this->preserve(
-                $data->getEveApiXml(),
-                $char['characterID'],
-                $aPreserver,
-                $vPreserver
-            );
-            $this->updateCachedUntil($data, $interval, $char['characterID']);
-        }
-    }
-    /**
-     * @return array
-     */
-    protected function getActiveCharacters()
-    {
-        $sql = $this->csq->getActiveRegisteredCharacters($this->getMask());
-        $this->getLogger()
-             ->debug($sql);
-        try {
-            $stmt = $this->getPdo()
-                         ->query($sql);
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $exc) {
-            $mess = 'Could NOT get a list of active characters';
-            $this->getLogger()
-                 ->warning($mess, array('exception' => $exc));
-            return array();
-        }
-    }
     /**
      * @return string
      */
     protected function getApiName()
     {
-        if (empty($this->apiName)) {
-            $this->apiName = basename(str_replace('\\', '/', __CLASS__));
-        }
-        return $this->apiName;
-    }
-    /**
-     * @return int
-     */
-    protected function getMask()
-    {
-        return $this->mask;
+        return 'CharacterInfo';
     }
     /**
      * @return string
@@ -180,38 +55,20 @@ class CharacterInfoPrivate extends AbstractCommonEveApi
         return $this->sectionName;
     }
     /**
-     * @param string                     $xml
-     * @param string                     $ownerID
-     * @param DatabasePreserverInterface $aPreserver
-     * @param DatabasePreserverInterface $vPreserver
+     * @param string $xml
+     * @param string $ownerID
      *
      * @return self
      */
     protected function preserve(
         $xml,
-        $ownerID,
-        DatabasePreserverInterface $aPreserver = null,
-        DatabasePreserverInterface $vPreserver = null
+        $ownerID
     ) {
-        if (is_null($aPreserver)) {
-            $aPreserver = new AttributesDatabasePreserver(
-                $this->getPdo(),
-                $this->getLogger(),
-                $this->getCsq()
-            );
-        }
-        if (is_null($vPreserver)) {
-            $vPreserver = new ValuesDatabasePreserver(
-                $this->getPdo(),
-                $this->getLogger(),
-                $this->getCsq()
-            );
-        }
         try {
             $this->getPdo()
                  ->beginTransaction();
-            $this->preserverToCharacterInfo($vPreserver, $xml);
-            $this->preserverToEmploymentHistory($aPreserver, $xml, $ownerID);
+            $this->preserverToCharacterInfo($xml);
+            $this->preserverToEmploymentHistory($xml, $ownerID);
             $this->getPdo()
                  ->commit();
         } catch (PDOException $exc) {
@@ -229,13 +86,11 @@ class CharacterInfoPrivate extends AbstractCommonEveApi
         return $this;
     }
     /**
-     * @param DatabasePreserverInterface $vPreserver
      * @param string                     $xml
      *
      * @return self
      */
     protected function preserverToCharacterInfo(
-        DatabasePreserverInterface $vPreserver,
         $xml
     ) {
         $columnDefaults = array(
@@ -258,20 +113,19 @@ class CharacterInfoPrivate extends AbstractCommonEveApi
             'lastKnownLocation' => '',
             'securityStatus' => '0'
         );
-        $vPreserver->setTableName('eveCharacterInfo')
-                   ->setColumnDefaults($columnDefaults)
-                   ->preserveData($xml);
+        $this->getValuesDatabasePreserver()
+             ->setTableName('eveCharacterInfo')
+             ->setColumnDefaults($columnDefaults)
+             ->preserveData($xml);
         return $this;
     }
     /**
-     * @param DatabasePreserverInterface $aPreserver
      * @param string                     $xml
      * @param string                     $ownerID
      *
      * @return self
      */
     protected function preserverToEmploymentHistory(
-        DatabasePreserverInterface $aPreserver,
         $xml,
         $ownerID
     ) {
@@ -281,13 +135,14 @@ class CharacterInfoPrivate extends AbstractCommonEveApi
             'startDate' => null,
             'ownerID' => $ownerID
         );
-        $aPreserver->setTableName('eveEmploymentHistory')
-                   ->setColumnDefaults($columnDefaults)
-                   ->preserveData($xml, '//employmentHistory/row');
+        $this->getAttributesDatabasePreserver()
+             ->setTableName('eveEmploymentHistory')
+             ->setColumnDefaults($columnDefaults)
+             ->preserveData($xml, '//employmentHistory/row');
         return $this;
     }
     /**
      * @var int $mask
      */
-    private $mask = 16777216;
+    protected $mask = 16777216;
 }

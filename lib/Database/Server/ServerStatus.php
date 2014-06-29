4a33d4e055a18b2ed929abc32477e0c848d0e7bc
stephenmg12 <stephenmg12@gmail.com>
2014-06-29 13:02:49 -0500

    adding Database/Server/ServerStatus.php

---
+++ lib/Database/Server/ServerStatus.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ * Contains ServerStatus class.
+ *
+ * PHP version 5.3
+ *
+ * LICENSE:
+ * This file is part of 1.1.x-WIP
+ * Copyright (C) 2014 Michael Cummings
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * You should be able to find a copy of this license in the LICENSE.md file. A copy of the GNU GPL should also be
+ * available in the GNU-GPL.md file.
+ *
+ * @copyright 2014 Michael Cummings
+ * @license   http://www.gnu.org/copyleft/lesser.html GNU LGPL
+ * @author    Michael Cummings <mgcummings@yahoo.com>
+ * @author    Stephen Gulick <stephenmg12@gmail.com>
+ */
+namespace Yapeal\Database\Server;
+
+use PDOException;
+use Yapeal\Database\AbstractCommonEveApi;
+use Yapeal\Xml\EveApiPreserverInterface;
+use Yapeal\Xml\EveApiReadWriteInterface;
+use Yapeal\Xml\EveApiRetrieverInterface;
+use Yapeal\Xml\EveApiXmlModifyInterface;
+
+/**
+ * Class ServerStatus
+ *
+ */
+class ServerStatus extends AbstractCommonEveApi
+{
+    /**
+     * @param EveApiReadWriteInterface $data
+     * @param EveApiRetrieverInterface $retrievers
+     * @param EveApiPreserverInterface $preservers
+     * @param int                      $interval
+     */
+    public function autoMagic(
+        EveApiReadWriteInterface $data,
+        EveApiRetrieverInterface $retrievers,
+        EveApiPreserverInterface $preservers,
+        $interval
+    ) {
+        $this->getLogger()
+             ->debug(
+             sprintf(
+                 'Starting autoMagic for %1$s/%2$s',
+                 $this->getSectionName(),
+                 $this->getApiName()
+             )
+            );
+        $data->setEveApiSectionName(strtolower($this->getSectionName()))
+             ->setEveApiName($this->getApiName());
+        if ($this->cacheNotExpired(
+                 $this->getApiName(),
+                     $this->getSectionName()
+        )
+        ) {
+            return;
+        }
+        if (!$this->oneShot($data, $retrievers, $preservers)) {
+            return;
+        }
+        $this->updateCachedUntil($data, $interval, 0);
+    }
+    /**
+     * @return string
+     */
+    protected function getSectionName()
+    {
+        if (empty($this->sectionName)) {
+            $this->sectionName = basename(str_replace('\\', '/', __DIR__));
+        }
+        return $this->sectionName;
+    }
+    /**
+     * @param EveApiReadWriteInterface $data
+     * @param EveApiRetrieverInterface $retrievers
+     * @param EveApiPreserverInterface $preservers
+     *
+     * @return bool
+     */
+    public function oneShot(
+        EveApiReadWriteInterface &$data,
+        EveApiRetrieverInterface $retrievers,
+        EveApiPreserverInterface $preservers
+    ) {
+        if (!$this->gotApiLock($data)) {
+            return false;
+        }
+        /**
+         * @var EveApiReadWriteInterface|EveApiXmlModifyInterface $data
+         */
+        $retrievers->retrieveEveApi($data);
+        if ($data->getEveApiXml() === false) {
+            $mess = sprintf(
+                'Could NOT retrieve Eve Api data for %1$s/%2$s',
+                strtolower($this->getSectionName()),
+                $this->getApiName()
+            );
+            $this->getLogger()
+                 ->notice($mess);
+            return false;
+        }
+        $this->xsltTransform($data);
+        if ($this->isInvalid($data)) {
+            $mess = sprintf(
+                'Data retrieved is invalid for %1$s/%2$s',
+                strtolower($this->getSectionName()),
+                $this->getApiName()
+            );
+            $this->getLogger()
+                 ->warning($mess);
+            $data->setEveApiName('Invalid' . $this->getApiName());
+            $preservers->preserveEveApi($data);
+            return false;
+        }
+        $preservers->preserveEveApi($data);
+        $this->preserve(
+             $data->getEveApiXml()
+        );
+        return true;
+    }
+    protected function getApiName()
+    {
+        if (empty($this->apiName)) {
+            $this->apiName = basename(str_replace('\\', '/', __CLASS__));
+        }
+        return $this->apiName;
+    }
+    /**
+     * @param string $xml
+     *
+     * @return self
+     */
+    protected function preserve(
+        $xml
+    ) {
+        try {
+            $this->getPdo()
+                 ->beginTransaction();
+            $this->preserverToServerStatus(
+                 $xml
+            );
+            $this->getPdo()
+                 ->commit();
+        } catch (PDOException $exc) {
+            $mess = sprintf(
+                'Failed to upsert data from Eve API %1$s/%2$s',
+                strtolower($this->getSectionName()),
+                $this->getApiName()
+            );
+            $this->getLogger()
+                 ->warning($mess, array('exception' => $exc));
+            $this->getPdo()
+                 ->rollBack();
+        }
+        return $this;
+    }
+    /**
+     * @param string $xml
+     *
+     * @return self
+     */
+    protected function preserverToServerStatus(
+        $xml
+    ) {
+        $columnDefaults = array(
+            'serverOpen' => 'False',
+            'onlinePlayers' => '0'
+        );
+        $this->getValuesDatabasePreserver()
+             ->setTableName('serverServerStatus')
+             ->setColumnDefaults($columnDefaults)
+             ->preserveData($xml);
+        return $this;
+    }
+}

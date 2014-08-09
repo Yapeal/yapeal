56635632ab05f8a5fea3e5ee6f0f302a5bccd2ca
Michael Cummings <mgcummings@yahoo.com>
2014-08-09 13:04:29 -0700

    Resolved issue #32 corpWalletTransactions not filling past Crius release

--- bin/sql/CreateCorpTables.sql
+++ bin/sql/CreateCorpTables.sql
@@ -525,7 +525,7 @@ CREATE TABLE IF NOT EXISTS "{database}"."{table_prefix}corpWalletTransactions" (
     "price"                DECIMAL(17, 2)       NOT NULL,
     "quantity"             BIGINT(20) UNSIGNED  NOT NULL,
     "stationID"            BIGINT(20) UNSIGNED           DEFAULT NULL,
-    "stationName"          CHAR(50)                      DEFAULT NULL,
+    "stationName"          CHAR(255)                     DEFAULT NULL,
     "transactionDateTime"  DATETIME             NOT NULL,
     "transactionFor"       CHAR(12)             NOT NULL DEFAULT 'corporation',
     "transactionID"        BIGINT(20) UNSIGNED  NOT NULL,

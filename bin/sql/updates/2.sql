b556dc22be65191d1efb162fee869adf6762fbc4
Michael Cummings <mgcummings@yahoo.com>
2014-08-20 11:38:56 -0700

    Updated eve/CharacterInfo for corporationName being added into employment history in update SQL.

--- bin/sql/updates/2.sql
+++ bin/sql/updates/2.sql
@@ -29,3 +29,6 @@ ON DUPLICATE KEY UPDATE
     "interval"    = VALUES("interval"),
     "isActive"    = VALUES("isActive");
 COMMIT;
+ALTER TABLE "{database}"."{table_prefix}eveEmploymentHistory"
+ADD COLUMN "corporationName" CHAR(50) NULL DEFAULT NULL
+AFTER "corporationID";

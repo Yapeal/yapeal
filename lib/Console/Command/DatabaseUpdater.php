bb64da93220119393cee3076107d3a7cbb6d7b75
Michael Cummings <mgcummings@yahoo.com>
2014-08-24 16:24:56 -0700

    Added Console/Command/DatabaseUpdater class.

--- lib/Console/Command/DatabaseUpdater.php
+++ lib/Console/Command/DatabaseUpdater.php
@@ -30,13 +30,16 @@
 namespace Yapeal\Console\Command;
 
 use DirectoryIterator;
-use mysqli;
+use PDO;
+use PDOException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Yaml\Exception\ParseException;
-use Symfony\Component\Yaml\Yaml;
+use Yapeal\Configuration\ConsoleWiring;
+use Yapeal\Container\ContainerInterface;
+use Yapeal\Database\CommonSqlQueries;
+use Yapeal\Exception\YapealDatabaseException;
 use Yapeal\Exception\YapealNormalizerException;
 use Yapeal\Filesystem\FilePathNormalizer;
 
@@ -48,19 +51,22 @@ class DatabaseUpdater extends Command
     /**
      * @param string|null $name
      * @param string      $cwd
+     * @param ContainerInterface $dic
      *
      * @throws \InvalidArgumentException
      * @throws \LogicException
      */
-    public function __construct($name = null, $cwd)
+    public function __construct($name = null, $cwd, ContainerInterface $dic)
     {
         $this->setDescription(
             'Retrieves SQL from files and updates database'
         );
         $this->setName($name);
         $this->setCwd($cwd);
+        $this->setDic($dic);
         parent::__construct($name);
     }
+
     /**
      * @param string $value
      *
@@ -76,6 +82,44 @@ class DatabaseUpdater extends Command
         $this->cwd = $value;
         return $this;
     }
+
+    /**
+     * @param ContainerInterface $value
+     *
+     * @return self
+     */
+    public function setDic(ContainerInterface $value)
+    {
+        $this->dic = $value;
+        return $this;
+    }
+    /**
+     * @param ContainerInterface $dic
+     *
+     * @throws YapealDatabaseException
+     */
+    public function wire(ContainerInterface $dic)
+    {
+        if (empty($dic['Yapeal.cwd'])) {
+            $dic['Yapeal.cwd'] = $this->getNormalizedPath($this->getCwd());
+        }
+        if (empty($dic['Yapeal.baseDir'])) {
+            $dic['Yapeal.baseDir'] =
+                str_replace('\\', '/', dirname(dirname(dirname(__DIR__))))
+                . '/';
+        }
+        $wiring = new ConsoleWiring($dic);
+        $wiring->wireDefaults()
+               ->wireConfiguration();
+        $dic['Yapeal.Config.Parser'];
+        $wiring->wireErrorLogger();
+        $dic['Yapeal.Error.Logger'];
+        $wiring->wireLogLogger()
+               ->wireDatabase()
+               ->wireCommonSqlQueries()
+               ->wireRetriever()
+               ->wirePreserver();
+    }
     /**
      * Configures the current command.
      */
@@ -87,48 +131,48 @@ class DatabaseUpdater extends Command
             InputOption::VALUE_REQUIRED,
             'Configuration file to get settings from.'
         )
-             ->addOption(
-                 'database',
-                 'd',
-                 InputOption::VALUE_REQUIRED,
-                 'Name of the database.'
-             )
-             ->addOption(
-                 'hostName',
-                 'o',
-                 InputOption::VALUE_REQUIRED,
-                 'Host name for database server.'
-             )
-             ->addOption(
-                 'password',
-                 'p',
-                 InputOption::VALUE_REQUIRED,
-                 'Password used to access database.'
-             )
-             ->addOption(
-                 'platform',
-                 null,
-                 InputOption::VALUE_REQUIRED,
-                 'Platform of database driver. Currently only "mysql".'
-             )
-             ->addOption(
-                 'port',
-                 null,
-                 InputOption::VALUE_REQUIRED,
-                 'Port number for remote server. Only needed if using http connection.'
-             )
-             ->addOption(
-                 'tablePrefix',
-                 't',
-                 InputOption::VALUE_REQUIRED,
-                 'Prefix for database table names.'
-             )
-             ->addOption(
-                 'userName',
-                 'u',
-                 InputOption::VALUE_REQUIRED,
-                 'User name used to access database.'
-             );
+            ->addOption(
+                'database',
+                'd',
+                InputOption::VALUE_REQUIRED,
+                'Name of the database.'
+            )
+            ->addOption(
+                'hostName',
+                'o',
+                InputOption::VALUE_REQUIRED,
+                'Host name for database server.'
+            )
+            ->addOption(
+                'password',
+                'p',
+                InputOption::VALUE_REQUIRED,
+                'Password used to access database.'
+            )
+            ->addOption(
+                'platform',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Platform of database driver. Currently only "mysql".'
+            )
+            ->addOption(
+                'port',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Port number for remote server. Only needed if using http connection.'
+            )
+            ->addOption(
+                'tablePrefix',
+                't',
+                InputOption::VALUE_REQUIRED,
+                'Prefix for database table names.'
+            )
+            ->addOption(
+                'userName',
+                'u',
+                InputOption::VALUE_REQUIRED,
+                'User name used to access database.'
+            );
         $help = <<<'HELP'
 The <info>%command.full_name%</info> command is used to initialize (create) a new
  database and tables to be used by Yapeal. If you already have a
@@ -149,6 +193,7 @@ You can also use the command before setting up a configuration file like so:
 HELP;
         $this->setHelp($help);
     }
+
     /**
      * Executes the current command.
      *
@@ -157,7 +202,7 @@ HELP;
      * execute() method, you set the code to execute by passing
      * a Closure to the setCode() method.
      *
-     * @param InputInterface  $input  An InputInterface instance
+     * @param InputInterface $input An InputInterface instance
      * @param OutputInterface $output An OutputInterface instance
      *
      * @return null|int     null or 0 if everything went fine, or an error code
@@ -167,88 +212,39 @@ HELP;
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $options = $input->getOptions();
-        $configFile = $this->getCwd() . '/config/yapeal.yaml';
-        if (!empty($options['configFile'])) {
-            $configFile =
-                $this->getNormalizedPath($options['configFile']);
-        }
-        $config = [];
-        if ($this->isAccessibleConfigFile($configFile, $output)) {
-            $mess = sprintf(
-                'Using configuration file %1$s for settings',
-                $configFile
-            );
-            $output->writeln($mess);
-            $config =
-                $this->getConfigFileSettings($configFile)['Yapeal']['Database'];
-        }
-        $config = array_replace(
-            ['tablePrefix' => ''],
-            $config
-        );
-        foreach ($options as $key => $value) {
-            if ($value !== null) {
-                $config[$key] = $value;
-            }
-        }
-        $required = ['database', 'hostName', 'password', 'userName'];
-        $mess = [];
-        foreach ($required as $setting) {
-            if (empty($config[$setting])) {
-                $mess[] =
-                    '<error>Missing required setting ' . $setting . '</error>';
-            }
-        }
-        if (!empty($mess)) {
-            $output->writeln($mess);
-            exit(1);
-        }
-        //var_dump($config);
-        //exit(0);
-        $this->processSql($this->getDbCon($config, $output), $config, $output);
+        $this->processCliOptions($input->getOptions());
+        $this->wire($this->getDic());
+        $this->processSql($output);
         return;
     }
+
     /**
-     * @param string $configFile
-     *
-     * @throws ParseException
-     * @return array
-     */
-    protected function getConfigFileSettings($configFile)
-    {
-        $yaml = new Yaml();
-        return $yaml->parse(file_get_contents($configFile));
-    }
-    /**
-     * @param mysqli          $mysqli
-     * @param array           $options
      * @param OutputInterface $output
      *
      * @return string
      */
-    protected function getCurrentDatabaseVersion(
-        mysqli $mysqli,
-        array $options,
-        OutputInterface $output
-    ) {
-        $sql = sprintf(
-            'SELECT "version" FROM "%1$s"."%2$sutilDatabaseVersion"',
-            $options['database'],
-            $options['tablePrefix']
-        );
-        $result = $mysqli->query($sql, MYSQLI_NUM);
-        if ($result === false) {
+    protected function getCurrentDatabaseVersion(OutputInterface $output)
+    {
+        /**
+         * @var CommonSqlQueries $csq
+         */
+        $csq = $this->getDic()['Yapeal.Database.CommonQueries'];
+        $sql = $csq->getUtilCurrentDatabaseVersion();
+        try {
+            $result = $this->getPdo($output)
+                           ->query($sql, PDO::FETCH_NUM);
+            $version = $result->fetchColumn();
+            //$output->writeln($version);
+            $result->closeCursor();
+        } catch (PDOException $exc) {
             $mess =
-                '<warning>Could NOT get database version using 0000</warning>';
-            $output->writeln($mess);
-            $mysqli->close();
-            return ['0000'];
+                '<warning>Could NOT get database version using default 197001010001</warning>';
+            $output->writeln([$sql, $mess]);
+            $version = '197001010001';
         }
-        $version = $result->fetch_row();
-        $result->close();
-        return (string)$version[0];
+        return sprintf('%1$012d', $version);
     }
+
     /**
      * @return string
      */
@@ -256,33 +252,15 @@ HELP;
     {
         return $this->cwd;
     }
+
     /**
-     * @param array           $options
-     * @param OutputInterface $output
-     *
-     * @return mysqli
+     * @return ContainerInterface
      */
-    protected function getDbCon(array $options, OutputInterface $output)
+    protected function getDic()
     {
-        if (is_null($this->mysqli)) {
-            $this->mysqli = @new mysqli(
-                $options['hostName'],
-                $options['userName'],
-                $options['password'],
-                ''
-            );
-            if (mysqli_connect_error()) {
-                $mess = sprintf(
-                    '<error>Could NOT connect to MySQL. MySQL error was (%1$s) %2$s</error>',
-                    mysqli_connect_errno(),
-                    mysqli_connect_error()
-                );
-                $output->writeln($mess);
-                exit(2);
-            }
-        }
-        return $this->mysqli;
+        return $this->dic;
     }
+
     /**
      * @param string $path
      *
@@ -293,6 +271,29 @@ HELP;
     {
         $fpn = new FilePathNormalizer();
         return $fpn->normalizePath($path);
+    }
+
+    /**
+     * @param OutputInterface $output
+     *
+     * @return PDO
+     */
+    protected function getPdo(OutputInterface $output)
+    {
+        if (is_null($this->pdo)) {
+            try {
+                $this->pdo = $this->getDic()['Yapeal.Database.Connection'];
+            } catch (PDOException $exc) {
+                $mess = sprintf(
+                    '<error>Could NOT connect to database. Database error was (%1$s) %2$s</error>',
+                    $exc->getCode(),
+                    $exc->getMessage()
+                );
+                $output->writeln($mess);
+                exit(2);
+            }
+        }
+        return $this->pdo;
     }
     /**
      * @param OutputInterface $output
@@ -318,45 +319,58 @@ HELP;
             if ($fileInfo->getExtension() !== 'sql') {
                 continue;
             }
+            //$output->writeln(str_replace('\\', '/', $fileInfo->getPathname()));
             $fileNames[] = str_replace('\\', '/', $fileInfo->getPathname());
         }
         return $fileNames;
     }
+
     /**
-     * @param string          $configFile
-     * @param OutputInterface $output
+     * @param array $options
      *
-     * @return bool
+     * @return self
      */
-    protected function isAccessibleConfigFile(
-        $configFile,
-        OutputInterface $output
-    ) {
-        if (is_readable($configFile) && is_file($configFile)) {
-            return true;
+    protected function processCliOptions(array $options)
+    {
+        $base = 'Yapeal.Database.';
+        foreach ([
+            'class',
+            'database',
+            'hostName',
+            'password',
+            'platform',
+            'tablePrefix',
+            'userName'
+        ] as $option) {
+            if (!empty($options[$option])) {
+                $this->getDic()[$base . $option] = $options[$option];
+            }
         }
-        $mess = sprintf(
-            '<warning>Was given %1$s which is not accessible</warning>',
-            $configFile
-        );
-        $output->writeln($mess);
-        return false;
+        if (!empty($options['configFile'])) {
+            $this->getDic()['Yapeal.Config.configDir'] =
+                dirname($options['configFile']);
+            $this->getDic()['Yapeal.Config.fileName'] =
+                basename($options['configFile']);
+        }
+        return $this;
     }
+
     /**
-     * @param mysqli          $mysqli
-     * @param array           $options
      * @param OutputInterface $output
      */
     protected function processSql(
-        mysqli $mysqli,
-        $options,
         OutputInterface $output
     ) {
-        $templates = [';', '{database}', '{table_prefix}'];
+        $templates = [';', '{database}', '{table_prefix}', '$$'];
         $replacements =
-            ['', $options['database'], $options['tablePrefix']];
-        $currentVersion =
-            $this->getCurrentDatabaseVersion($mysqli, $options, $output);
+            [
+                '',
+                $this->getDic()['Yapeal.Database.database'],
+                $this->getDic()['Yapeal.Database.tablePrefix'],
+                ';'
+            ];
+        $pdo = $this->getPdo($output);
+        $currentVersion = $this->getCurrentDatabaseVersion($output);
         foreach ($this->getUpdateFileList($output) as $fileName) {
             if (!is_file($fileName)) {
                 $mess = sprintf(
@@ -366,7 +380,13 @@ HELP;
                 $output->writeln($mess);
                 continue;
             }
-            if (basename($fileName) <= $currentVersion) {
+            if (basename($fileName, '.sql') <= $currentVersion) {
+                $mess = sprintf(
+                    '<info>Skipping SQL file %1$s <= database current version %2$s</info>',
+                    basename($fileName),
+                    $currentVersion
+                );
+                $output->writeln($mess);
                 continue;
             }
             $sqlStatements = file_get_contents($fileName);
@@ -384,68 +404,81 @@ HELP;
             // Replace {database}, {table_prefix}, and ';' in statements.
             $sqlStatements =
                 str_replace($templates, $replacements, $sqlStatements);
-            foreach ($sqlStatements as $line => $sql) {
+            foreach ($sqlStatements as $statement => $sql) {
                 $sql = trim($sql);
                 // 5 is a 'magic' number that I think is shorter than any sql statement.
                 if (strlen($sql) < 5) {
                     continue;
                 }
-                if ($mysqli->query($sql) === false) {
-                    $mess[] = sprintf(
-                        '<error>Sql statement failed in %1$s on line %2$s with (%3$s) %4$s</error>',
+                try {
+                    //$output->writeln($sql);
+                    $pdo->exec($sql);
+                } catch (PDOException $exc) {
+                    $mess = sprintf(
+                        '<error>Sql failed in %1$s on statement %2$s with (%3$s) %4$s</error>',
                         $fileName,
-                        $line,
-                        $mysqli->errno,
-                        $mysqli->error
+                        $statement,
+                        $exc->getCode(),
+                        $exc->getMessage()
                     );
-                    $mess[] = $sql;
-                    $output->writeln($mess);
-                    $mysqli->close();
+                    $output->writeln([$sql, $mess]);
                     exit(2);
                 }
                 $output->write('.');
             }
-            $currentVersion = basename($fileName);
-            $this->updateCurrentDatabaseVersion(
-                $currentVersion,
-                $mysqli,
-                $options,
-                $output
-            );
+            $currentVersion = basename($fileName, '.sql');
+            $this->updateCurrentDatabaseVersion($currentVersion, $output);
             $output->writeln('');
         }
     }
     /**
      * @param string          $currentVersion
-     * @param mysqli          $mysqli
-     * @param array           $options
      * @param OutputInterface $output
+     *
+     * @return self
      */
     protected function updateCurrentDatabaseVersion(
         $currentVersion,
-        mysqli $mysqli,
-        array $options,
         OutputInterface $output
-    ) {
-        $sql = <<<'SQL'
-UPDATE "%1$s"."%2$sutilDatabaseVersion" SET "version"='%3$s'
-SQL;
-        $sql = sprintf($sql, $options['database'], $options['tablePrefix']);
-        if ($mysqli->query($sql) !== true) {
+    )
+    {
+        /**
+         * @var CommonSqlQueries $csq
+         */
+        $csq = $this->getDic()['Yapeal.Database.CommonQueries'];
+        $sql = $csq->getUtilCurrentDatabaseVersionUpdate($currentVersion);
+        //$mess =sprintf( 'Updating database version to %1$s',$currentVersion);
+        //$output->writeln([$sql, $mess]);
+        try {
+            $this->getPdo($output)
+                 ->beginTransaction();
+            $stmt = $this->getPdo($output)
+                         ->prepare($sql);
+            $stmt->execute([$currentVersion]);
+            $this->getPdo($output)
+                 ->commit();
+        } catch (PDOException $exc) {
             $mess = sprintf(
                 '<error>Database "version" update failed for %1$s</error>',
                 $currentVersion
             );
-            $output->writeln($mess);
+            $output->writeln([$sql, $mess]);
+            $this->getPdo($output)
+                 ->rollBack();
             exit(2);
         }
+        return $this;
     }
     /**
-     * @type string
+     * @type string $cwd
      */
     protected $cwd;
     /**
-     * @type mysqli $mysqli
+     * @type ContainerInterface $dic
      */
-    protected $mysqli;
+    protected $dic;
+    /**
+     * @type PDO $pdo
+     */
+    protected $pdo;
 }

335fb44a35694dda48fac35c5e2404124b5f698b
Michael Cummings <mgcummings@yahoo.com>
2014-09-05 12:52:21 -0700

    Resolved #46 eve/characterInfo doesn't allow UTF-8 character in shipName column

--- bin/sql/CreateUtilTables.sql
+++ bin/sql/CreateUtilTables.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS "{database}"."{table_prefix}utilDatabaseVersion" (
     DEFAULT CHARSET =ascii;
 INSERT INTO "{database}"."{table_prefix}utilDatabaseVersion" ("version")
 VALUES
-    ('197001010001');
+    ('201409051857');
 DROP TABLE IF EXISTS "{database}"."{table_prefix}utilEveApi";
 CREATE TABLE IF NOT EXISTS "{database}"."{table_prefix}utilEveApi" (
     "apiName"     CHAR(32)            NOT NULL,

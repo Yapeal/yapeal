e6d4fc2c563481f83569eb5d007ddc0e4f3699ea
Michael Cummings <mgcummings@yahoo.com>
2015-05-15 00:18:00 -0700

    Merged older SQL updates. Updated composer.json for newer versions of FPN and dropped codesniffer from require-dev.

--- lib/Sql/updates/201502120216.sql
+++ lib/Sql/updates/201502120216.sql
@@ -3,3 +3,8 @@ CALL "{database}"."AddOrModifyColumn"('{database}',
                                       '{table_prefix}charContracts',
                                       'volume',
                                       'DECIMAL(20,4) UNSIGNED NOT NULL AFTER "type"');
+-- Sql/update/201502121621.sql
+CALL "{database}"."AddOrModifyColumn"('{database}',
+                                      '{table_prefix}corpContracts',
+                                      'volume',
+                                      'DECIMAL(20,4) UNSIGNED NOT NULL AFTER "type"');
--- lib/Sql/updates/201502121621.sql
+++
@@ -1,5 +0,0 @@
--- Sql/update/201502121621.sql
-CALL "{database}"."AddOrModifyColumn"('{database}',
-                                      '{table_prefix}corpContracts',
-                                      'volume',
-                                      'DECIMAL(20,4) UNSIGNED NOT NULL AFTER "type"');

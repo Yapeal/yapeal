d414b081f59228d6dd403a40978d4d83be92d92d
Michael Cummings <mgcummings@yahoo.com>
2015-06-13 10:12:41 -0700

    Updated ContainerInterface to make Scrutinizer happier.

--- lib/Container/ContainerInterface.php
+++ lib/Container/ContainerInterface.php
@@ -33,8 +33,6 @@
  */
 namespace Yapeal\Container;
 
-use \ArrayAccess;
-
 /**
  * Interface ContainerInterface
  *
@@ -46,7 +44,7 @@ use \ArrayAccess;
  *
  * @since 2.0.0-alpha5
  */
-interface ContainerInterface extends ArrayAccess
+interface ContainerInterface
 {
     /**
      * Extends an object definition.
@@ -106,4 +104,58 @@ interface ContainerInterface extends ArrayAccess
      * @throws \InvalidArgumentException if the identifier is NOT defined
      */
     public function raw($key);
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Whether a offset exists
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     *
+     * @param mixed $offset <p>
+     *                      An offset to check for.
+     *                      </p>
+     *
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     */
+    public function offsetExists($offset);
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Offset to retrieve
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to retrieve.
+     *                      </p>
+     *
+     * @return mixed Can return all value types.
+     */
+    public function offsetGet($offset);
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Offset to set
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to assign the value to.
+     *                      </p>
+     * @param mixed $value  <p>
+     *                      The value to set.
+     *                      </p>
+     *
+     * @return void
+     */
+    public function offsetSet($offset, $value);
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Offset to unset
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to unset.
+     *                      </p>
+     *
+     * @return void
+     */
+    public function offsetUnset($offset);
 }
